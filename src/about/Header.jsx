f8826ffc1c6c8f5b1b9e929c2678a9a2cab1b792
Abiey2579 <mbyahya2579@gmail.com>
2022-07-03 13:22:16 -0700

    Saving some Random Changes

--- src/about/Header.jsx
+++ src/about/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="About-Header">
       <h5 className="Header-Pre">HEY THERE! I AM</h5>
-      <h2 className="Founder-Name">Yahaya Muhammad Bello</h2>
+      <h2 className="Founder-Name">Yahaya</h2>
       <h5 className="Header-Post">And This Is My Resume</h5>
     </header>
   );
