9a20fa49786cc21dff842c28d5d287edafe8bf9b
Abiey2579 <mbyahya2579@gmail.com>
2022-07-08 14:04:01 -0700

    Finished Updating About Me Header

--- src/AboutMe/AboutHeader.jsx
+++ src/AboutMe/AboutHeader.jsx
@@ -5,9 +5,14 @@ const AboutHeader = () => {
   return (
     <header className="About-Header">
       <div className="About-Header-Overlay">
-        <div className="About-Yahaya-Picture-4-4"></div>
-        <h2 className="About-Yahaya-Name">Yahaya Muhammad</h2>
-        <h5 className="About-Yahaya-Profession">Full-Stack Web Developer</h5>
+        {/* <div className="About-Yahaya-Picture-4-4"></div> */}
+        <h2 className="About-Yahaya-Name">Yahaya Muhammad Bello</h2>
+        <h5 className="About-Yahaya-Profession">
+          A Creative Freelancer & Full-Stack Developer
+        </h5>
+        <button className="About-Yahaya-Hire-Me x-btn x-btn-warning">
+          Hire Me
+        </button>
       </div>
     </header>
   );
