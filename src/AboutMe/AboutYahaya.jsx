05d97f7776f092429137753969ed568657873ef2
Abiey2579 <mbyahya2579@gmail.com>
2022-07-11 06:29:35 -0700

    I updated About Yahaya Section, but still needs more

--- src/AboutMe/AboutYahaya.jsx
+++ src/AboutMe/AboutYahaya.jsx
@@ -1,66 +1,32 @@
 import React from "react";
 import "./css/AboutYahaya.css";
-import AboutYahayaPicture from "./image/About-Yahaya-Picture.jpg";
-import AboutYahayaPsychometric from "./documents/ABOUT-YAHAYA-MUHAMMAD-BELLO-Psychometric.pdf";
+import AboutYahayaPicture44 from "./image/About-Yahaya-Picture-4-4.jpg";
 
 const AboutYahaya = () => {
   return (
-    <div className="Xyshma-Founder container-fluid row">
-      <div className="Founder-Image-Container col-md-5 mb-5">
+    <div className="About-Yahaya-Section container row">
+      <div className="About-Yahaya-Image-Container col-md-4">
         <img
-          src={AboutYahayaPicture}
-          className="Founder-Image"
-          alt="Xyshma Founder Image"
+          src={AboutYahayaPicture44}
+          className="About-Yahaya-Image"
+          alt="About Yahaya Image"
         />
       </div>
-      <div className="Founder-Info col-md-5">
-        <h2 className="Founder-Info-Head">About Me</h2>
-        <p className="Founder-Info-Preamble">
-          I am a qualified Web Developer mostly deals with Frontend Web
-          Development, with 5 years experience.This includes HTML5, CSS3,
-          JavaScript, Bootstrap, Jquery, React JS, PHP, Python, MySQL.
+      <div className="About-Yahaya-Info-Container col-md-6">
+        <h2 className="About-Yahaya-Info-Head">Hi there, I'm Yahaya</h2>
+        <h2 className="About-Yahaya-Info-SubHead">Full-stack Web Developer</h2>
+        <p className="About-Yahaya-Info-Preamble">
+          I'm an expert developer, with 5+ years experience in designing and
+          developing user interface, testing, debugging and tracing staff within
+          e-commerce technologies. Proven ability in optimizing web performance
+          and functionalities that improve data retrieval and workflow
+          efficiencies.
         </p>
-        <table className="Founder-Info-Table">
-          <tr>
-            <td>
-              <i className="fi fi-person fi-fw Founder-Info-Icon"></i>
-            </td>
-            <td className="Founder-Info-Content">Yahaya Muhammad Bello</td>
-          </tr>
-          <tr>
-            <td>
-              <i className="fi fi-calendar fi-fw Founder-Info-Icon"></i>
-            </td>
-            <td className="Founder-Info-Content">
-              3<sup>rd</sup> January, 2002
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <i className="fa fa-phone-alt fa-fw Founder-Info-Icon"></i>
-            </td>
-            <td className="Founder-Info-Content">+234 9039402857</td>
-          </tr>
-          <tr>
-            <td>
-              <i className="fi fi-email fi-fw Founder-Info-Icon"></i>
-            </td>
-            <td className="Founder-Info-Content">iiabba2579@gmail.com</td>
-          </tr>
-          <tr>
-            <td>
-              <i className="fi fi-home fi-fw Founder-Info-Icon"></i>
-            </td>
-            <td className="Founder-Info-Content">Borno State, Nigeria</td>
-          </tr>
-        </table>
-        <a
-          href={AboutYahayaPsychometric}
-          className="x-btn x-btn-primary Founder-Info-Download"
-          target="_blank"
-        >
-          <i className="fa fa-cloud-download-alt mr-2 fa-fw"></i>
-          Download Psychometric
+        <a href="" className="x-btn x-btn-warning mr-2">
+          My Resume
+        </a>
+        <a href="" className="x-btn x-btn-warning">
+          My Psychometic
         </a>
       </div>
     </div>
